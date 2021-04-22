45fefe429b39bd72c2879ca78de4c28f652ba678
Your Name <camilovelasquez2003@gmail.com>
2021-04-21 22:49:39 -0500

    cuarta parte de ejercicios

---
+++ Arreglo.java
@@ -0,0 +1,26 @@
+public class Arreglo{
+public static void main(String[]args){
+int arreglo[];
+arreglo = new int[200];
+
+arreglo[]int = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+/*arreglo[0]= 4;
+arreglo[1]=34;
+arreglo[2]=12;
+arreglo[3]=8;
+arreglo[4]=-2;
+arreglo[5]
+arreglo[6]
+arreglo[7]
+arreglo[8]*/
+
+
+ /*for(int i=0; i<arreglo ; i++){ //Para imprimir los valores que son menores que 5.
+System.out.println(arreglo[i]);
+}*/
+
+/*for (int i:arreglo) {
+	System.out.println(i);
+}*/
+}
+}
---
+++ Otroarreglo.java
@@ -0,0 +1,16 @@
+public class Otroarreglo{
+public static void main(String[]args){
+int arreglo[];
+arreglo = new int[5];
+
+ arreglo[0]= 4;
+arreglo[1]=34;
+arreglo[2]=12;
+arreglo[3]=8;
+arreglo[4]=-2;
+
+ for(int i=0; i<5 ; i++){ //Para imprimir los valores que son menores que 5.
+System.out.println(i);
+}
+}
+}
