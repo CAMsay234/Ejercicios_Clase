4d0d294dc0da8264c234000d818aad9c1fdfbbeb
Your Name <camilovelasquez2003@gmail.com>
2021-04-21 22:46:23 -0500

    tercera parte de ejercicios

---
+++ Calcular.java
@@ -0,0 +1,156 @@
+public class Calcular{
+	public static void main(String[] args) {
+		
+		int numero1 = 500;
+		System.out.println(numero1);
+		int numero2 = 5000;
+		System.out.println(numero2);
+		int numero3 = -400;
+		System.out.println(numero3);
+		int numero4 = -200;
+		System.out.println(numero4);
+		int numero5 = 10000;
+		System.out.println(numero5);
+		int numero6 = -430;
+		System.out.println(numero6);
+		int numero7 = -1;
+		System.out.println(numero7);
+		int numero8 = -400;
+		System.out.println(numero8);
+		int numero9 = 2300;
+		System.out.println(numero9);
+		int numero10 = 4000;
+		System.out.println(numero10);
+		System.out.println();
+
+		if (numero1 < 0)
+		{
+		 	System.out.println("número1 negativo");
+		}
+		if (100 < numero1)
+		{
+			System.out.println("número1 mayor que 100");
+		}
+		if (5000 < numero1)
+		{
+			System.out.println("número1 mayor que 5000");
+		}
+		
+		if(numero2 < 0)
+		{
+			System.out.println("número2 negativo");
+		}
+		if (100 < numero2)
+		{
+			System.out.println("número2 mayor que 100");
+		}
+		if (5000 < numero2)
+		{
+			System.out.println("número2 mayor que 5000");
+		}
+			
+		if (numero3 < 0)
+		{
+			System.out.println("número3 negativo");
+		}
+		if (100 < numero3)
+		{
+	   		System.out.println("número3 mayor que 100");
+		}
+		if (5000 < numero3)
+		{
+			System.out.println("número3 mayor que 5000");
+		}
+				
+		if (numero4 < 0)
+		{
+			System.out.println("número4 negativo");
+		}
+		if (100< numero4)
+		{
+			System.out.println("número4 mayor que 100");
+		}
+		if (5000 < numero4)
+		{
+			System.out.println("número4 mayor que 5000");
+		}
+					
+		if(numero5 < 0)
+		{
+			System.out.println("número5 negativo");
+		}
+		if (100 < numero5)
+		{
+			System.out.println("número5 mayor que 100");
+		}
+		if (5000 < numero5)
+		{
+			System.out.println("número5 mayor que 5000");
+		}
+						
+		if (numero6 <0)
+		{
+			System.out.println("número6 negativo");
+		}
+		if (100<numero6)
+		{
+			System.out.println("número6 mayor que 100");
+		}
+		if (5000 < numero6)
+		{
+			System.out.println("número6 mayor que 5000");
+		}
+
+		if (numero7<0)
+		{
+			System.out.println("número7 negativo");
+		}
+		if (100<numero7)
+		{
+			System.out.println("número7 mayor que 100");
+		}
+		if (5000<numero7)
+		{
+			System.out.println("número7 mayor que 5000");
+		}
+
+		if (numero8 < 0)
+		{
+			System.out.println("número8 negativo");
+		}
+		if (100<numero8)
+		{
+			System.out.println("número8 mayor que 100");
+		}
+		if (5000<numero8)
+		{
+			System.out.println("número8 mayor que 5000");
+		}
+
+		if(numero9<0)
+		{
+			System.out.println("número9 negativo");
+		}
+		if (100<numero9)
+		{
+			System.out.println("número9 mayor que 100");
+		}
+		if (5000<numero9)
+		{
+			System.out.println("número9 mayor que 5000");
+		}
+		if (numero10< 0)
+		{
+			System.out.println("número10 negativo");
+		}
+		if (100<numero10)
+		{
+			System.out.println("número10 mayor que 100");
+		}
+		if (5000<numero10)
+		{
+		System.out.println("número10 mayor que 5000");
+		}
+										}
+									}
+										
---
+++ OperadorTernario.java
@@ -0,0 +1,6 @@
+public class OperadorTernario{
+	public static void main(String[] args) {
+		/System.out.println((Math.random()>=.5)?"Cara":"Sello");
+
+	}
+}
